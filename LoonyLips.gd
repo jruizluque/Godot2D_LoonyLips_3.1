9d0f3df1242d8c82924d2b67f27916e6504d23d0
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2019-07-08 17:51:59 -0300

    Label - Showing Text to the Player

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,8 +3,5 @@ extends Control
 func _ready():
 	var prompts = ["Yann", "Minions", "greatest", "two"]
 	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past %s decades"
-	print(story % prompts)
-	
-	prompts = ["Rick", "Cinderella", "worst", "three"]
-	print(story % prompts)
-	
+
+	$DisplayText.text = story % prompts
