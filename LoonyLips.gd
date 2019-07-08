29c6789c5e368e65bdaa54dc51ef1b4e4de27457
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2019-07-08 18:51:55 -0300

    LineEdit - Getting Text From the Player

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,5 +3,13 @@ extends Control
 func _ready():
 	var prompts = ["Yann", "Minions", "greatest", "two"]
 	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past %s decades"
+	$VBoxContainer/DisplayText.text = story % prompts
 
-	$DisplayText.text = story % prompts
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/PlayerText.clear()
